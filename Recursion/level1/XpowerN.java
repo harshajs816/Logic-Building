b6f45237bd169d6e81884730917178fc73f69b54
harsh yadav <officialharshajs@gmail.com>
2026-07-25 20:08:15 +0530

    add new in  basic recursion

---
+++ Recursion/level1/XpowerN.java
@@ -0,0 +1,19 @@
+
+
+public class XpowerN {
+
+    public static int power(int x ,int n){
+
+        if (n==1) {
+            return x;
+        }
+
+        return x*power(x, n-1);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println( power(2,10));
+    }
+    
+}
