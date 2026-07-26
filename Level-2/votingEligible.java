b62f0bf80a56d783a5972d0a8fc3ae615a90ca07
harsh yadav <officialharshajs@gmail.com>
2026-07-26 23:40:28 +0530

    add new in level 2

--- Level-2/votingEligible.java
+++ Level-2/votingEligible.java
@@ -1,3 +1,20 @@
 public class votingEligible {
+
+    public static void eligible(int age){
+         if(age>=18){
+            System.out.println("You are eligible for voating");
+         }
+         else if (age<=0){
+            System.out.println("Enter Correct age");
+         }
+         else{
+            System.out.println("You are not eligible for voating");
+         }
+    }
+
+    public static void main(String[] args) {
+        int age = -1;
+        eligible(age);
+    }
     
 }
