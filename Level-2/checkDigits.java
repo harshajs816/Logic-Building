5fca52467ea81a19dfbd62b8c3f430a962266825
harsh yadav <officialharshajs@gmail.com>
2026-07-26 23:59:15 +0530

    add new in level 2

---
+++ Level-2/checkDigits.java
@@ -0,0 +1,16 @@
+public class checkDigits {
+
+
+    public static void digit(int a, int b, int c){
+        if( a==b && b==c){
+            System.out.println("digits are eqaul");
+        }
+        else{
+            System.out.println("digits are not equal");
+        }
+    }
+    public static void main(String[] args) {
+        digit(10,20,30);
+    }
+    
+}
