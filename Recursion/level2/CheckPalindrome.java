c6f27291aa5e1f43689c91b13d8fd7a1bfe52ba4
harsh yadav <officialharshajs@gmail.com>
2026-07-29 23:39:26 +0530

    add more in level2 recursion

--- Recursion/level2/CheckPalindrome.java
+++ Recursion/level2/CheckPalindrome.java
@@ -1,14 +1,24 @@
 public class CheckPalindrome {
+    static int rev=0;
 
-    public static void palindrome(int num){
+    public static int palindrome(int num){
 
-
-        int lastDigit = (num % 10);
-        int digit = num;
+        if(num==0){
+            return rev;
+        }
+        int digit = (num % 10);
+         rev = rev * 10 + digit;
+         
+         return palindrome(num/10);
     }
     
 
     public static void main(String[] args) {
-        
+
+        int num = 11211;
+    
+        if(num==palindrome(num)){
+            System.out.println("Number is Palindrome");
+        }
     }
 }
