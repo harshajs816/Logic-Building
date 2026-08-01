934f78f284c9fb514d86e3522332cfca0e63f6a4
harsh yadav <officialharshajs@gmail.com>
2026-08-01 23:26:54 +0530

    add new file in looping folder

--- looping-pattern/level1/Printeven.java
+++ looping-pattern/level1/Printeven.java
@@ -3,7 +3,11 @@ public class Printeven {
     public static void main(String[] args) {
         
         for(int i =1; i<=100;i++){
-            
+
+            if(i%2==0){
+                System.out.print(i+" ");
+                
+            }
         }
 
     }
