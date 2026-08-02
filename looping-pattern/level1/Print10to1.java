cf8fccea8d0eacb941e41d458664aad0579ab4f1
harsh yadav <officialharshajs@gmail.com>
2026-08-02 23:29:53 +0530

    add in looping-pattern

---
+++ looping-pattern/level1/Print10to1.java
@@ -0,0 +1,8 @@
+public class Print10to1{
+    public static void main(String[] args) {
+        
+        for(int num = 10; num >=1 ; num--){
+            System.out.print(num + " ");
+        }
+    }
+}
