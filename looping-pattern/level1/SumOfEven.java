17f7ac726f4a3e34df3611b222de7b42e6533730
harsh yadav <officialharshajs@gmail.com>
2026-08-03 23:04:28 +0530

    add new in level1 looping

--- looping-pattern/level1/SumOfEven.java
+++ looping-pattern/level1/SumOfEven.java
@@ -2,22 +2,26 @@ import java.util.Scanner;
 
 public class SumOfEven {
 
+    public static int sumOfEv( int num , int sum ){
+
+     for(int i=0; i <= num; i++){
+           
+        if(i%2==0){
+            sum = sum + i;
+        }
+     }
+     return sum;
+    }
+
     public static void main(String[] args) {
         
       Scanner sc = new Scanner(System.in);
  
       System.out.print("Enter Number :");
       int num = sc.nextInt();
-
-         for(int i=1;i<=num ;i++){
-
-             if(i%2==0){
-            System.out.print(i + " ");
-        }
-        
-
-    
-        }
+      int sum =0;
+ System.out.println(sumOfEv(num, sum));
+         
        
     }
     
