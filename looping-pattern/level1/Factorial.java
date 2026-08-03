d7f691b54fabc4f21e42e9cf98190e5101907661
harsh yadav <officialharshajs@gmail.com>
2026-08-03 23:21:19 +0530

    add new in level1 looping

---
+++ looping-pattern/level1/Factorial.java
@@ -0,0 +1,15 @@
+public class Factorial {
+
+   
+    public static void main(String[] args) {
+        int num=20;
+
+        for(int i=1;i<=num;i++){
+            
+            if(num % i == 0){
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+}
