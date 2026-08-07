c8f37d04e930f2d988e3e7264c61b90eadac88f8
harsh yadav <officialharshajs@gmail.com>
2026-08-07 23:01:01 +0530

    add BasicArray folder

---
+++ BasicArray/SumOfArray.java
@@ -0,0 +1,10 @@
+public class SumOfArray{
+   public static void main(String[] args) {
+    int a[]={1,2,3,4,5};
+    int sum =0;
+    for(int i=0; i<=a.length-1;i++){
+        sum = sum+a[i];
+    }
+    System.out.print(sum);
+   }
+}
